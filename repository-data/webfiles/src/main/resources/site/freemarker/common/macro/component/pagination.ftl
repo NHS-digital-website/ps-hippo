NHS-digital-website/hippo

[DW-1188] Automate publication + create early access

repository-data/webfiles/src/main/resources/site/freemarker/common/macro/component/pagination.ftl
@@ -1,7 +1,7 @@
 <#ftl output_format="HTML">
 
 <#-- Note Page must implement uk.nhs.digital.pagination.Paginated -->
-<#macro pagination page>
+<#macro pagination page earlyAccessKey="">
     <#assign pagenation = page.paginate() />
     <#if pagination??>
         <nav class="digital-pagination" aria-label="Pagination">
@@ -9,7 +9,9 @@
                 <#if pagenation.previous??>
                     <li class="digital-pagination-list-item-previous">
                         <a class="digital-pagination-link"
-                           href="<@hst.link hippobean=pagenation.previous.linkedBean />">
+                           href="<@hst.link hippobean=pagenation.previous.linkedBean>
+                                    <#if earlyAccessKey?has_content><@hst.param name="key" value="${earlyAccessKey}"/></#if>
+                                 </@hst.link>">
                             <span class="digital-pagination-link-title"><img aria-hidden="true" alt="Left Arrow" src="<@hst.webfile path="/images/pagination/left-arrow.svg"/>"/> Previous</span>
                             <span class="digital-pagination-link-hidden"> : </span>
                             <span class="digital-pagination-link-page">${pagenation.previous.title}</span>
@@ -19,7 +21,9 @@
                 <#if pagenation.next??>
                     <li class="digital-pagination-list-item-next">
                         <a class="digital-pagination-link"
-                           href="<@hst.link hippobean=pagenation.next.linkedBean />">
+                           href="<@hst.link hippobean=pagenation.next.linkedBean>
+                                    <#if earlyAccessKey?has_content><@hst.param name="key" value="${earlyAccessKey}"/></#if>
+                                 </@hst.link>">
                             <span class="digital-pagination-link-title">Next <img aria-hidden="true" alt="Right Arrow" src="<@hst.webfile path="/images/pagination/right-arrow.svg"/>"/></span>
                             <span class="digital-pagination-link-hidden"> : </span>
                             <span class="digital-pagination-link-page">${pagenation.next.title}</span>

site/components/src/main/java/uk/nhs/digital/ps/beans/PublicationBase.java
@@ -4,6 +4,7 @@ import static java.util.Arrays.asList;
 import static org.apache.commons.collections.IteratorUtils.toList;
 
 import com.google.common.collect.Lists;
+import org.apache.commons.lang.StringUtils;
 import org.hippoecm.hst.container.RequestContextProvider;
 import org.hippoecm.hst.content.beans.query.HstQuery;
 import org.hippoecm.hst.content.beans.query.HstQueryResult;
@@ -20,11 +21,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.nhs.digital.common.util.DocumentUtils;
 import uk.nhs.digital.ps.components.DocumentTitleComparator;
+import uk.nhs.digital.ps.directives.DateFormatterDirective;
 import uk.nhs.digital.ps.site.exceptions.DataRestrictionViolationException;
 import uk.nhs.digital.website.beans.News;
 import uk.nhs.digital.website.beans.SupplementaryInformation;
 import uk.nhs.digital.website.beans.Update;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -35,10 +38,13 @@ public abstract class PublicationBase extends BaseDocument {
     private static final Collection<String> propertiesPermittedWhenUpcoming = asList(
         PropertyKeys.TITLE,
         PropertyKeys.NOMINAL_DATE,
-        PropertyKeys.PUBLICLY_ACCESSIBLE,
         PropertyKeys.INFORMATION_TYPE,
         PropertyKeys.PARENT_SERIES
     );
+
+    public static final int HOUR_OF_PUBLIC_RELEASE = 9;
+    public static final int MINUTE_OF_PUBLIC_RELEASE = 30;
+    public static final String EARLY_ACCESS_KEY_QUERY_PARAM = "key";
 
     private RestrictableDate nominalPublicationDate;
 
@@ -184,7 +190,7 @@ public abstract class PublicationBase extends BaseDocument {
     }
 
     public boolean isPubliclyAccessible() {
-        return getPropertyIfPermitted(PropertyKeys.PUBLICLY_ACCESSIBLE);
+        return !getBeforePublicationDate() || isCorrectAccessKey();
     }
 
     @HippoEssentialsGenerated(internalName = PropertyKeys.RELATED_LINKS)
@@ -237,13 +243,29 @@ public abstract class PublicationBase extends BaseDocument {
         ).collect(Collectors.toList());
     }
 
+    public Boolean getBeforePublicationDate() {
+        Calendar publicationDate = getProperty(PropertyKeys.NOMINAL_DATE);
+        if (publicationDate == null) {
+            return false;
+        }
+        LocalDateTime publicationDateTime = publicationDate.toInstant()
+            .atZone(DateFormatterDirective.TIME_ZONE.toZoneId()).toLocalDateTime()
+            .withHour(HOUR_OF_PUBLIC_RELEASE).withMinute(MINUTE_OF_PUBLIC_RELEASE)
+            .withSecond(0);
+
+        LocalDateTime currentDateTime = LocalDateTime
+            .now(DateFormatterDirective.TIME_ZONE.toZoneId());
+
+        return currentDateTime.isBefore(publicationDateTime);
+    }
+
     @Override
     protected void assertPropertyPermitted(final String propertyKey) {
-
         final boolean isPropertyPermitted =
             isPropertyAlwaysPermitted(propertyKey)
                 || isPubliclyAccessible()
-                || propertiesPermittedWhenUpcoming.contains(propertyKey);
+                || propertiesPermittedWhenUpcoming.contains(propertyKey)
+                || isCorrectAccessKey();
 
         if (!isPropertyPermitted) {
             throw new DataRestrictionViolationException(
@@ -252,9 +274,17 @@ public abstract class PublicationBase extends BaseDocument {
         }
     }
 
+    public boolean isCorrectAccessKey() {
+        return StringUtils
+            .isNotBlank(getProperty(PublicationBase.PropertyKeys.EARLY_ACCESS_KEY))
+            && getProperty(PublicationBase.PropertyKeys.EARLY_ACCESS_KEY).equals(
+            RequestContextProvider.get().getServletRequest().getParameter(
+                EARLY_ACCESS_KEY_QUERY_PARAM));
+    }
+
     private boolean isPropertyAlwaysPermitted(final String propertyKey) {
         return PropertyKeys.PARENT_BEAN.equals(propertyKey)
-            || PropertyKeys.PUBLICLY_ACCESSIBLE.equals(propertyKey);
+            || PropertyKeys.EARLY_ACCESS_KEY.equals(propertyKey);
     }
 
     interface PropertyKeys {
@@ -274,10 +304,10 @@ public abstract class PublicationBase extends BaseDocument {
         String GRANULARITY = "publicationsystem:Granularity";
         String ADMINISTRATIVE_SOURCES = "publicationsystem:AdministrativeSources";
         String TITLE = "publicationsystem:Title";
-        String PUBLICLY_ACCESSIBLE = "publicationsystem:PubliclyAccessible";
         String RELATED_LINKS = "publicationsystem:RelatedLinks";
         String RESOURCE_LINKS = "publicationsystem:ResourceLinks";
         String ATTACHMENTS_V3 = "publicationsystem:Attachments-v3";
+        String EARLY_ACCESS_KEY = "publicationsystem:earlyaccesskey";
 
         String PARENT_BEAN = "PARENT_BEAN";
         String PARENT_SERIES = "PARENT_SERIES";

NHS-digital-website/ps-hippo

repository-data/webfiles/src/main/resources/site/freemarker/common/macro/component/pagination.ftl
@@ -1,7 +1,7 @@
 <#ftl output_format="HTML">
 
 <#-- Note Page must implement uk.nhs.digital.pagination.Paginated -->
-<#macro pagination page>
+<#macro pagination page earlyAccessKey="">
     <#assign pagination = page.paginate() />
     <#if (pagination)?has_content>
         <nav class="digital-pagination" aria-label="Pagination">
@@ -9,7 +9,9 @@
                 <#if (pagination.previous)?has_content>
                     <li class="digital-pagination-list-item-previous">
                         <a class="digital-pagination-link"
-                           href="<@hst.link hippobean=pagination.previous.linkedBean />">
+                           href="<@hst.link hippobean=pagination.previous.linkedBean>
+                                    <#if earlyAccessKey?has_content><@hst.param name="key" value="${earlyAccessKey}"/></#if>
+                                 </@hst.link>">
                             <span class="digital-pagination-link-title"><img aria-hidden="true" alt="Left Arrow" src="<@hst.webfile path="/images/pagination/left-arrow.svg"/>"/> Previous</span>
                             <span class="digital-pagination-link-hidden"> : </span>
                             <span class="digital-pagination-link-page">${pagination.previous.title}</span>
@@ -19,7 +21,9 @@
                 <#if (pagination.next)?has_content>
                     <li class="digital-pagination-list-item-next">
                         <a class="digital-pagination-link"
-                           href="<@hst.link hippobean=pagination.next.linkedBean />">
+                           href="<@hst.link hippobean=pagination.next.linkedBean>
+                                    <#if earlyAccessKey?has_content><@hst.param name="key" value="${earlyAccessKey}"/></#if>
+                                 </@hst.link>">
                             <span class="digital-pagination-link-title">Next <img aria-hidden="true" alt="Right Arrow" src="<@hst.webfile path="/images/pagination/right-arrow.svg"/>"/></span>
                             <span class="digital-pagination-link-hidden"> : </span>
                             <span class="digital-pagination-link-page">${pagination.next.title}</span>

site/components/src/main/java/uk/nhs/digital/ps/beans/PublicationBase.java
@@ -4,6 +4,7 @@ import static java.util.Arrays.asList;
 import static org.apache.commons.collections.IteratorUtils.toList;
 
 import com.google.common.collect.Lists;
+import org.apache.commons.lang.StringUtils;
 import org.hippoecm.hst.container.RequestContextProvider;
 import org.hippoecm.hst.content.beans.query.HstQuery;
 import org.hippoecm.hst.content.beans.query.HstQueryResult;
@@ -20,11 +21,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.nhs.digital.common.util.DocumentUtils;
 import uk.nhs.digital.ps.components.DocumentTitleComparator;
+import uk.nhs.digital.ps.directives.DateFormatterDirective;
 import uk.nhs.digital.ps.site.exceptions.DataRestrictionViolationException;
 import uk.nhs.digital.website.beans.News;
 import uk.nhs.digital.website.beans.SupplementaryInformation;
 import uk.nhs.digital.website.beans.Update;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -35,10 +38,13 @@ public abstract class PublicationBase extends BaseDocument {
     private static final Collection<String> propertiesPermittedWhenUpcoming = asList(
         PropertyKeys.TITLE,
         PropertyKeys.NOMINAL_DATE,
-        PropertyKeys.PUBLICLY_ACCESSIBLE,
         PropertyKeys.INFORMATION_TYPE,
         PropertyKeys.PARENT_SERIES
     );
+
+    public static final int HOUR_OF_PUBLIC_RELEASE = 9;
+    public static final int MINUTE_OF_PUBLIC_RELEASE = 30;
+    public static final String EARLY_ACCESS_KEY_QUERY_PARAM = "key";
 
     private RestrictableDate nominalPublicationDate;
 
@@ -184,7 +190,7 @@ public abstract class PublicationBase extends BaseDocument {
     }
 
     public boolean isPubliclyAccessible() {
-        return getPropertyIfPermittedSingle(PropertyKeys.PUBLICLY_ACCESSIBLE);
+        return !getBeforePublicationDate() || isCorrectAccessKey();
     }
 
     @HippoEssentialsGenerated(internalName = PropertyKeys.RELATED_LINKS)
@@ -237,13 +243,29 @@ public abstract class PublicationBase extends BaseDocument {
         ).collect(Collectors.toList());
     }
 
+    public Boolean getBeforePublicationDate() {
+        Calendar publicationDate = getPropertyIfPermittedSingle(PropertyKeys.NOMINAL_DATE);
+        if (publicationDate == null) {
+            return false;
+        }
+        LocalDateTime publicationDateTime = publicationDate.toInstant()
+            .atZone(DateFormatterDirective.TIME_ZONE.toZoneId()).toLocalDateTime()
+            .withHour(HOUR_OF_PUBLIC_RELEASE).withMinute(MINUTE_OF_PUBLIC_RELEASE)
+            .withSecond(0);
+
+        LocalDateTime currentDateTime = LocalDateTime
+            .now(DateFormatterDirective.TIME_ZONE.toZoneId());
+
+        return currentDateTime.isBefore(publicationDateTime);
+    }
+
     @Override
     protected void assertPropertyPermitted(final String propertyKey) {
-
         final boolean isPropertyPermitted =
             isPropertyAlwaysPermitted(propertyKey)
                 || isPubliclyAccessible()
-                || propertiesPermittedWhenUpcoming.contains(propertyKey);
+                || propertiesPermittedWhenUpcoming.contains(propertyKey)
+                || isCorrectAccessKey();
 
         if (!isPropertyPermitted) {
             throw new DataRestrictionViolationException(
@@ -252,9 +274,17 @@ public abstract class PublicationBase extends BaseDocument {
         }
     }
 
+    public boolean isCorrectAccessKey() {
+        return StringUtils
+            .isNotBlank(getProperty(PublicationBase.PropertyKeys.EARLY_ACCESS_KEY))
+            && getProperty(PublicationBase.PropertyKeys.EARLY_ACCESS_KEY).equals(
+            RequestContextProvider.get().getServletRequest().getParameter(
+                EARLY_ACCESS_KEY_QUERY_PARAM));
+    }
+
     private boolean isPropertyAlwaysPermitted(final String propertyKey) {
         return PropertyKeys.PARENT_BEAN.equals(propertyKey)
-            || PropertyKeys.PUBLICLY_ACCESSIBLE.equals(propertyKey);
+            || PropertyKeys.EARLY_ACCESS_KEY.equals(propertyKey);
     }
 
     interface PropertyKeys {
@@ -274,10 +304,10 @@ public abstract class PublicationBase extends BaseDocument {
         String GRANULARITY = "publicationsystem:Granularity";
         String ADMINISTRATIVE_SOURCES = "publicationsystem:AdministrativeSources";
         String TITLE = "publicationsystem:Title";
-        String PUBLICLY_ACCESSIBLE = "publicationsystem:PubliclyAccessible";
         String RELATED_LINKS = "publicationsystem:RelatedLinks";
         String RESOURCE_LINKS = "publicationsystem:ResourceLinks";
         String ATTACHMENTS_V3 = "publicationsystem:Attachments-v3";
+        String EARLY_ACCESS_KEY = "publicationsystem:earlyaccesskey";
 
         String PARENT_BEAN = "PARENT_BEAN";
         String PARENT_SERIES = "PARENT_SERIES";